12055e9eadf403da1a969bcef62ce379068e1790
Vincent Tung <vt167098@hotmail.com>
2021-12-01 22:44:40 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-[purple]">
+    Tailwind JIT mode!!
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
