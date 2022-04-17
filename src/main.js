dc84b1097d92ff6d62373f78b234fb2cdc146df0
Vincent Tung <vt167098@hotmail.com>
2022-04-17 20:02:34 +0800

    Update main.js
    變更theme為tailwind

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 
-import 'primevue/resources/themes/saga-blue/theme.css';
+//import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/themes/tailwind-light/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';          
 import './index.css'
